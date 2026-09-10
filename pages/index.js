b22ddb47262335c267b486383d400e2ec41870e8
emilioremache-business <emilio.remache@sikkercsc.com>
2026-09-10 16:19:33 -0500

    fix: change modalidad text to Microsoft Teams

--- pages/index.js
+++ pages/index.js
@@ -91,7 +91,7 @@ export default function Home() {
             </div>
             <div style={styles.eventDetailItem}>
               <span style={styles.eventDetailLabel}>💻 Modalidad</span>
-              <span style={styles.eventDetailValue}>Virtual, desde donde estés</span>
+              <span style={styles.eventDetailValue}>Microsoft Teamsés</span>
             </div>
           </div>
 
